c482c25ceed825825082c5fd9201811ac5f9df76
bhanuchander <bhanuchander210@gmail.com>
2019-09-15 07:40:18 +0530

    app update

--- application.py
+++ application.py
@@ -5,37 +5,37 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     html = '''
-<div align="center">
-<h1 style="color:blue;"> Samsung Electronics Stock Prediction Prediction </h1>
-<h3 >Predicted Value : </h3>
-<h5 style="color:red;">GRU : 46784.2532709241</h5>
-<h5 style="color:red;">LSTM : 46699.87167716027</h5>
-</div>
-
-<div align="center">
-    <h3 >Trend</h3>
-    <img src="outputs/final.png"/>
-</div>
-
-<h3 >Techniques Used</h3>
-<ul>
-    <li>Recurrent Neural Network</li>
-    <li>NLP</li>
-</ul>
-
-<h3>Observation</h3>
-<h3>GRU</h3>
-<h5>Loss : 0.176</h5>
-<h5>Loss / Learning Trend</h5>
-<img src="outputs/gru_4.png"/>
-
-<h3>LSTM</h3>
-<h5>Loss : 0.172</h5>
-<h5>Loss / Learning Trend</h5>
-<img src="outputs/lstm_4.png"/>
-
-<h2>To Predict the current / Intra day prediction checkout 'test.py' GitHub:</h2>
-<a href="https://github.com/Bhanuchander210/devengers"></a>
+    <div align="center">
+    <h1 style="color:blue;"> Samsung Electronics Stock Prediction Prediction </h1>
+    <h3 >Predicted Value : </h3>
+    <h5 style="color:red;">GRU : 46784.2532709241</h5>
+    <h5 style="color:red;">LSTM : 46699.87167716027</h5>
+    </div>
+    
+    <div align="center">
+        <h3 >Trend</h3>
+        <img src="outputs/final.png"/>
+    </div>
+    
+    <h3 >Techniques Used</h3>
+    <ul>
+        <li>Recurrent Neural Network</li>
+        <li>NLP</li>
+    </ul>
+    
+    <h3>Observation</h3>
+    <h3>GRU</h3>
+    <h5>Loss : 0.176</h5>
+    <h5>Loss / Learning Trend</h5>
+    <img src="https://github.com/Bhanuchander210/devengers/blob/master/outputs/gru_4.png"/>
+    
+    <h3>LSTM</h3>
+    <h5>Loss : 0.172</h5>
+    <h5>Loss / Learning Trend</h5>
+    <img src="https://github.com/Bhanuchander210/devengers/blob/master/outputs/lstm_4.png"/>
+    
+    <h2>To Predict the current / Intra day prediction checkout 'test.py' GitHub:</h2>
+    <a href="https://github.com/Bhanuchander210/devengers"></a>
     '''
     return html
 
