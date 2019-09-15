28302f235a1007cc95693a14ba7dd41ed8f1154e
bhanuchander <bhanuchander210@gmail.com>
2019-09-15 05:44:39 +0530

    app update

--- application.py
+++ application.py
@@ -1,22 +1,40 @@
 from flask import Flask
 from keras.models import load_model
-import pickle
+from flask import request
+import pickle, numpy as np
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello, SmartNinja!"
-
-
-@app.route("/test")
-def test(inval):
-    return "this is our inval ; " + inval
+    return "Hi there... Itachi here...!"
 
 
 @app.route("/predict")
-def newhello():
-    test = []
+def predict():
+    open = request.args.get('open')
+    high = request.args.get('high')
+    low = request.args.get('low')
+    volume = request.args.get('volume')
+    open_aapl = request.args.get('open_aapl')
+    high_aapl = request.args.get('high_aapl')
+    low_aapl = request.args.get('low_aapl')
+    open_goog = request.args.get('open_goog')
+    high_goog = request.args.get('high_goog')
+    low_goog = request.args.get('low_goog')
+    open_intc = request.args.get('open_intc')
+    high_intc = request.args.get('high_intc')
+    low_intc = request.args.get('low_intc')
+    open_lg = request.args.get('open_lg')
+    high_lg = request.args.get('high_lg')
+    low_lg = request.args.get('low_lg')
+    sentiment = request.args.get('sentiment')
+    subjectivity = request.args.get('subjectivity')
+
+    test = [open,high,low,volume,open_aapl,high_aapl,low_aapl,open_goog,high_goog,low_goog,open_intc,high_intc, low_intc,open_lg, high_lg, low_lg, sentiment, subjectivity, 0]
+    scaler = pickle.load(open("models/scaler.pkl", 'rb'))
+    test = scaler.transform(test)[0][:-1]
+    test = np.reshape(test, (1, 1, len(test)))
     model = load_model('models/GRU_4.h5')
     predicted = model.predict(test)
     return str(predicted)
--- corr.py
+++ corr.py
@@ -11,31 +11,37 @@ drop_cols = ['Adj Close']
 margin = 0.3
 prefix = 'Close_'
 
-master_df = pd.read_csv(master_file)
-master_df = master_df.dropna()
-master_df = master_df.set_index('Date')
-master_df = master_df.sort_index()
 
-master_df = master_df.drop(columns=drop_cols)
+def main():
 
-for f in glob.glob("data/assoc/*.csv"):
-    temp = pd.read_csv(f, usecols= assoc_user_cols)
-    temp = temp.dropna()
-    temp = temp.set_index('Date')
-    temp = temp.sort_index()
-    temp.columns = [(str(col) + '_' + os.path.basename(f)).replace('.csv', '') for col in temp.columns]
-    master_df = master_df.join(temp)
+    master_df = pd.read_csv(master_file)
+    master_df = master_df.dropna()
+    master_df = master_df.set_index('Date')
+    master_df = master_df.sort_index()
+
+    master_df = master_df.drop(columns=drop_cols)
+
+    for f in glob.glob("data/assoc/*.csv"):
+        temp = pd.read_csv(f, usecols= assoc_user_cols)
+        temp = temp.dropna()
+        temp = temp.set_index('Date')
+        temp = temp.sort_index()
+        temp.columns = [(str(col) + '_' + os.path.basename(f)).replace('.csv', '') for col in temp.columns]
+        master_df = master_df.join(temp)
+
+    others = [x for x in list(master_df) if prefix in x]
+
+    eligible = []
+    for c in others:
+        val = master_df[target_col].corr(master_df[c])
+        val = abs(val)
+        print(c + ' Corr val : '+ str(val))
+        if val >= margin:
+            eligible.append(str(c).replace(prefix, ''))
+
+    out = pd.DataFrame({'eligible': eligible})
+    out.to_csv('data/eligible.csv')
 
 
-others = [x for x in list(master_df) if prefix in x]
-
-eligible = []
-for c in others:
-    val = master_df[target_col].corr(master_df[c])
-    val = abs(val)
-    print(c + ' Corr val : '+ str(val))
-    if val >= margin:
-        eligible.append(str(c).replace(prefix, ''))
-
-out = pd.DataFrame({'eligible': eligible})
-out.to_csv('data/eligible.csv')
+if __name__ == '__main__':
+    main()
